b9310285dffbcb15e1a9c641bf24ae2bdafb15b2
Qihong Lu <lvqihong1992@gmail.com>
2015-11-25 15:12:48 -0600

    just a re-run, after using col_normalize

--- runlasso.m
+++ runlasso.m
@@ -1,19 +1,21 @@
 % initialize 
 clear all;close all;clc
 % the data for the 1st subject, which includes the X matrix
-load('jlp01.mat')           
+load('data/jlp01.mat')           
 % there are 10 structs in this metadata, the 1st struct corresponds to the
 % 1st subject, which contains the y.
-load('jlp_metadata.mat')    
+load('data/jlp_metadata.mat')    
+[m,n] = size(X);    
 
 % here's how you would retrieve the y vector
 y = metadata(1).TrueFaces;  
 y(y == 0) = -1;
 
+%% normalize the features
 % m = numPics, n = numVoxels
-[m,n] = size(X);    
-Xn = X - mean(X(:)) / std(X(:));
+X_n = columnNormalization(X);
 
+%% 
 % cv
 testSize = 50;
 testIdx = false(m,1);
@@ -34,17 +36,17 @@ tau = .9 / norm(X,2)^2;
 tau_n = .9 / norm(Xn,2)^2;
 
 %% fit the model 
-[beta, record] = lasso_lsta(Xtrain, ytrain, lambda, tau, 0);
-[beta_n, record_n] = lasso_lsta(Xntrain, ytrain, lambda, tau_n, 0);
+[beta, record] = lasso_lsta(Xtrain, ytrain, lambda, tau, 1);
+[beta_n, record_n] = lasso_lsta(Xntrain, ytrain, lambda, tau_n, 1);
 
 % generate prediction  
 predict = sign(Xtest * beta(:,end)); 
 predict_n = sign(Xntest * beta_n(:,end)); 
 
 accuracy = sum(bsxfun(@eq, predict, ytest))/testSize
-accuracy = sum(bsxfun(@eq, predict_n, ytest))/testSize
+accuracy_n = sum(bsxfun(@eq, predict_n, ytest))/testSize
 
+%% print some results
 plot(beta(:,end), beta_n(:,end), 'o')
-
-
-norm(beta(:,end)-beta_n(:,end),1)
+fprintf('Difference in norm %f \n', norm(beta(:,end)-beta_n(:,end),1));
+fprintf('Number of zero weights (raw vs. nor): %d %d\n', sum(beta(:,end)==0), sum(beta_n(:,end)==0));
