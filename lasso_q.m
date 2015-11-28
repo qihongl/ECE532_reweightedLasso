d92957018a74705bba7fab6c708e6ad584d3434d
Qihong Lu <lvqihong1992@gmail.com>
2015-11-28 12:10:19 -0600

    implement gradient decent

--- lasso_q.m
+++ lasso_q.m
@@ -1,14 +1,14 @@
 %% iterative soft thresholding to lasso
-function [beta, record, finalBeta] = lasso_lsta(X, y, lambda, tau, display)
+function [finalBeta, history] = lasso_q(X, y, lambda, tau, display, algorithm)
 
 %% set some parameters
+[m,n] = size(X);
 maxIter = 10000;   % maxiteration that the program will run
 tolerance = 1e-5;
 
-%% compute things that need to be computed many times
-[m,n] = size(X);
-tau_XTy = tau * X' * y;
-tau_XTX = tau * X' * X;
+%% pre-compute things that need to be computed many times
+XTy = X' * y;
+XTX = X' * X;
 % preallocate
 beta = zeros(n,1);
 if display
@@ -16,19 +16,27 @@ if display
 end
 for i = 1 : maxIter;
     %% update weight
-    Z = beta(:,i) - tau_XTX * beta(:,i) +  tau_XTy;
-    beta(:,i+1) = sign(Z) .* max((Z - 2*lambda*tau), 0);
-    
+    if strcmp(algorithm,'gd')
+        % gradient decent 
+        grad = - 2 * XTy + 2 * XTX * beta(:,i) + lambda * sign(beta(:,i));
+        beta(:,i+1) = beta(:,i) -  tau * grad;
+    elseif strcmp(algorithm,'ista')
+        % iterative soft thresholding 
+        Z = beta(:,i) - tau*XTX * beta(:,i) +  tau*XTy;
+        beta(:,i+1) = sign(Z) .* max((Z - 2*lambda*tau), 0);
+    else
+        error('unrecognized input parameter');
+    end
     %% Performance monitoring
     % compute change in beta
     diff = norm(beta(:,i+1) - beta(:,i));
     % nnz for the updated beta
     nnz = n-numZeros(beta(:,i+1));
     % error of the updated beta
-    record.accuracy(i) = sum(sign(X * beta(:,i+1)) == y) / m;
+    history.accuracy(i) = sum(sign(X * beta(:,i+1)) == y) / m;
     % print the performance
     if display
-        fprintf('%4d%12.4f %16.4f %10d\n', i, record.accuracy(i), diff, nnz);
+        fprintf('%4d%12.4f %16.4f %10d\n', i, history.accuracy(i), diff, nnz);
     end
     
     %% stopping criterion
@@ -38,12 +46,12 @@ for i = 1 : maxIter;
 end
 
 finalBeta = beta(:,end);
-
+history.beta = beta;
 % record number of non-zero weights
-record.nonZeroBetas = nnz;
+history.nonZeroBetas = nnz;
 % plot the performance
 if display
-%     plotError(1-record.accuracy)
+    %     plotError(1-record.accuracy)
 end
 end
 
