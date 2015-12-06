18dad8a504e9b5266be7f52278d708ee6d5beccb
Qihong Lu <lvqihong1992@gmail.com>
2015-12-06 14:09:53 -0600

    some comments

--- lasso_ista.m
+++ lasso_ista.m
@@ -1,4 +1,12 @@
-%% optimize lasso with iterative soft thresholding
+%% lasso with iterative soft thresholding & landweber 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% input     X           m by n design matrix
+%           y           m by 1 responses 
+%           lambda(>0)  regularization parameter, 
+%           weights     n by 1 weights vector. (all 1 = regular lasso)
+%           display     show progress
+% output    beta        the lasso solution
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function [beta] = lasso_ista(X, y, lambda, weights, display)
 % constants
 MAX_ITERS = 1e4;     % maximum number of iterations % convergence tolerance
@@ -6,17 +14,17 @@ TOLERANCE = 1e-5;
 tau = .99 / norm(X)^2;  % choose stepsize
 [~,n] = size(X);
 
-beta = zeros(n,1);          % preallocate
-XTX = X' * X;               % precompute matrix operations
+% precompute repeated matrix operations
+XTX = X' * X;               
 XTy = X' * y;
 
 %% iterative soft-thresholding
+beta = zeros(n,1);          % preallocate
 for i = 1: MAX_ITERS
     % parameter update with landweber iteration
     z = beta - tau * (XTX * beta - XTy);
     betaPrev = beta;
     beta = sign(z) .* max( abs(z) - tau * lambda * weights / 2, 0 );
-    
     
     % display progress
     if display
@@ -26,7 +34,6 @@ for i = 1: MAX_ITERS
     if norm(beta - betaPrev) < TOLERANCE
         break
     end
-    
 end
 
 end
