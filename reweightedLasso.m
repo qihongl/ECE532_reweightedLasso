145ad4365e12fecdaf7820693dfb725f11a76498
Qihong Lu <lvqihong1992@gmail.com>
2015-12-06 14:10:08 -0600

    some comments

--- reweightedLasso.m
+++ reweightedLasso.m
@@ -1,14 +1,22 @@
-%% reweighted-lasso, by Emmanuel et al.
-function [beta, history] = reweightedLasso(X, y, lambda, display)
+%% reweighted-lasso
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% input     X           m by n design matrix
+%           y           m by 1 responses 
+%           display     show progress
+% output    beta        the lasso solution
+%           history     beta & weights over iterations 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function [beta, history] = reweightedLasso(X, y, display)
 % some constants
 MAX_ITER = 30;
 [~,n] = size(X);
+tolerance = 1e-6;
 
 %% iteratively fitting reweighted-lasso
 weights = ones(n,1);
 for i = 1 : MAX_ITER
     % fit lasso
-    beta = lasso_ista(X, y, lambda, weights, 0);
+    beta = lasso_ista(X, y, 1, weights, 0);
     % update weights
     e = .1;
     weights = 1 ./ (abs(beta) + e);
@@ -18,13 +26,13 @@ for i = 1 : MAX_ITER
     history.beta(:,i) = beta;
     
     if i > 1
-        if display
-            diff = norm(history.beta(:,i) - history.beta(:,i-1),2);
-            fprintf('%d\t%f\n',i,diff);
-        end
+        diff = norm(history.beta(:,i) - history.beta(:,i-1),2);
         % stopping critera
-        if diff < 1e-3
+        if diff < tolerance
             break;
+        end
+        if display  
+            fprintf('%d\t%f\n',i,diff);
         end
     end
     
