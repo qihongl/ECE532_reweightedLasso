692161ca42288e9cf8b1755dbe1c16874adf3079
Qihong Lu <lvqihong1992@gmail.com>
2015-12-05 23:42:07 -0600

    gen signal with a magnitude option

--- generateBeta.m
+++ generateBeta.m
@@ -3,12 +3,12 @@
 %           number of informative features
 %           the magnitude of the signal
 % output:   a beta vector that statisfy you choices
-function beta = generateBeta(numInfoFeatures, numTotalFeatures)
+function beta = generateBeta(numInfoFeatures, numTotalFeatures, magnitude)
 % generate the subset indices
 temp = randperm(numTotalFeatures);
 index = temp(1 : numInfoFeatures);
 % set most of the beta to zero
 beta = zeros(numTotalFeatures,1);
 % set a subset of them to be randn
-beta(index) = randn(size(index));
+beta(index) = randn(size(index))*magnitude;
 end
