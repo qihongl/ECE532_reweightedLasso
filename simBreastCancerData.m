67b38a10d3a4bd2dd69dced93a75adeb1e3a0178
Qihong Lu <lvqihong1992@gmail.com>
2015-12-08 21:00:18 -0600

    simulations with bc data

--- simBreastCancerData.m
+++ simBreastCancerData.m
@@ -1,47 +1,47 @@
 clear variables; close all; 
 load('data/BreastCancer.mat')
 
-[m,n] = size(X);
-Xn = columnNormalization(X);
-lambda = 10; 
-weights = ones(n,1);
+numFeatures = 512; 
+numEgs = 256;
+[M,N] = size(X);
+egidx = randsample(M,numEgs);
+X = X(egidx, randsample(N,numFeatures));
+y=y(egidx);
+[m,n] = size(X);  
 
-%% fit regular lasso vs. reweighed lasso
-[beta.std] = lasso_ista(X, y, lambda, weights, 0);
-[beta.rw] = reweightedLasso(X, y, lambda, 1);
 
-%% fit regular lasso vs. reweighed lasso (for normalized data)
-[beta.normal] = lasso_ista(Xn, y, lambda, weights, 0);
-[beta.normalrw] = reweightedLasso(Xn, y, lambda, 1);
+%% Cross validation
+% indices for the testing set
+testSize = 45;
+testIdx = false(m,1);
+testIdx(randsample(m,testSize)) = 1;
 
-%% find out performance
-accuracy.raw = sum(sign(X * beta.std) == y )/ length(y)
-accuracy.rawrw = sum(sign(X * beta.rw) == y )/ length(y)
+% split the data
+Xtrain = X(~testIdx,:);
+ytrain = y(~testIdx);
+Xtest = X(testIdx,:);
+ytest = y(testIdx);
 
-accuracy.normal = sum(sign(X * beta.normal) == y) / length(y)
-accuracy.normalrw = sum(sign(X * beta.normalrw) == y) / length(y)
 
-nnz(beta.raw)
-nnz(beta.rawrw)
 
-nnz(beta.normal)
-nnz(beta.normalrw)
+%% fitting reweighted-lasso 
+[beta.rw history] = reweightedLasso_cvx(Xtrain,ytrain);
 
-% without normalization 
-% accuracy  1   vs .94
-% sparsity 3666 vs 142
+beta.lasso = lasso_ista(Xtrain,ytrain,1,ones(n,1),0);
+
 
 %% plot
-subplot(2,1,1)
-plot(beta.raw)
-title('Beta estimation with lasso WITHOUT reweighting')
-subplot(2,1,2)
-plot(beta.rawrw)
-title('Beta estimation with lasso WITH reweighting')
+% if the truth signal have similar magnitude
+plot(beta.rw, beta.lasso, 'o')
+title('Compare rw and regular lasso')
 
-subplot(2,1,1)
-plot(beta.normal)
-title('Beta estimation with lasso WITHOUT reweighting')
-subplot(2,1,2)
-plot(beta.normalrw)
-title('Beta estimation with lasso WITH reweighting')
+sum(sign(Xtest * beta.rw) == ytest) / testSize
+sum(sign(Xtest * beta.lasso) == ytest)  / testSize
+
+numNonZeros(beta.rw)
+numNonZeros(beta.lasso)
+
+% 
+% L0 find a much sparser solution than lasso! 
+% 
+
